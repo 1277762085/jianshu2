69951369e9182fc61179824b647e427281cd71f0
1277762085 <cwh19961102>
2018-04-19 22:47:03 +0800

    头部文件第一天

---
+++ new/jianshu/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="default-tamplate">
+    <nuxt/>
+  
+  </div>
+</template>
+
+<script>
+
+export default{
+// 首先引入bootstrap。css的文件
+
+}
+</script>
+<style >
+body {
+  padding: 56px;
+  min-width: 768px;
+  font-size: 16px;
+}
+</style>
+
+
+
---
+++ new/jianshu/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  
+ <div style="height:100%">
+     <my-header></my-header>
+     <p>我的主体内容部分</p>
+ </div>
+
+
+</template>
+
+
+
+<script>
+import myHeader from '../components/myHeader'
+export default {
+  head: {
+    title: "简书-创作你得创作",
+    meta: [
+      { charset: "utf-8" },
+      { name: "keyword", content: "简书首页" },
+      { name: "description", content: "简书" },
+      { name: "author", content: "曹伟豪" }
+    ]
+  },
+  components:{
+      myHeader
+  },
+  data() {
+    return {
+      name: "首页"
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
